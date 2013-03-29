85899822705e82ddebf4da6d1218a24d61856a74
Kunal Kamble <kunalkamble@Kunal-Kambles-MacBook-Pro-2.local>
2013-03-29 07:17:14 -0400

    Mydemoapp - Initial files.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mytestapp::Application.config.secret_token = '9c2936e3348e0fba49f8f629e77e72e391dbd1816ef861505fb3faba68137eb152b1330705a314858be3ff9a4de063efdefc4847c42bd400ec72de96f08631ab'
